6b2073f07675d9088c0ad056444ac4e93cb5c6b8
Michael Erdely <mike@erdelynet.com>
2026-03-20 08:30:10 -0400

    Fix get_updates_available.py script for FlatpakUpdates to actually pull down new version info

--- FlatpakUpdates/scripts/get_updates_available.py
+++ FlatpakUpdates/scripts/get_updates_available.py
@@ -1,27 +1,40 @@
 #!/usr/bin/env python3
 
-import subprocess
 import json
+import subprocess
+import sys
+from urllib.error import URLError, HTTPError
+from urllib.request import urlopen
 
-cmd = [ 'env', 'FLATPAK_FANCY_OUTPUT=0', 'flatpak', 'list', '--columns=application,version' ]
+cmd = [ 'env', 'FLATPAK_FANCY_OUTPUT=0', 'flatpak', 'list', '--columns=name,application,version' ]
 output = subprocess.run(cmd, capture_output=True, text=True)
 
-applist = {}
+appname = {}
+appvers = {}
 
 for line in output.stdout.splitlines():
     arr = line.split('\t')
-    if len(arr) == 2:
-        applist[arr[0]] = arr[1]
+    if len(arr) == 3:
+        appname[arr[1]] = arr[0]
+        appvers[arr[1]] = arr[2]
 
-#print('{ "name": "Fake App", "id": "app.fake.App", "currentVersion": "1.0", "newVersion": "1.1" }')
-#quit()
+def get_latest(app_id: str) -> str:
+    try:
+        with urlopen(f"https://flathub.org/api/v2/appstream/{app_id}", timeout=10) as response:
+            content = response.read()
+            json_string = json.loads(content.decode("utf-8"))
+        return json_string['releases'][0]['version']
+    except HTTPError as e:
+        print(f"HTTP error: {e.code} {e.reason}", file=sys.stderr)
+    except URLError as e:
+        print(f"URL error: {e.reason}", file=sys.stderr)
+    except Exception as e:
+        print(f"Unexpected error: {e}", file=sys.stderr)
+    return ""
 
-output = ""
-cmd = [ 'env', 'FLATPAK_FANCY_OUTPUT=0', 'flatpak', 'remote-ls', '--updates', '--columns', 'name,application,version' ]
-result = subprocess.run(cmd, capture_output=True, text=True)
-if result.stdout:
-    for line in result.stdout.splitlines():
-        arr = line.split('\t')
-        if len(arr) == 3:
-            cur = applist[arr[1]]
-            print(f"{{ \"name\": \"{arr[0]}\", \"id\": \"{arr[1]}\", \"currentVersion\": \"{cur}\", \"newVersion\": \"{arr[2]}\" }}")
+for app in appname:
+    new_ver=get_latest(app)
+    if not new_ver or new_ver == appvers[app]:
+        continue
+    print(f"{{ \"name\": \"{appname[app]}\", \"id\": \"{app}\", \"currentVersion\": \"{appvers[app]}\", \"newVersion\": \"{new_ver}\" }}")
+
